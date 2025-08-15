19f176ad5f2fe01e0c90c02aa6bab53555cb4905
Sophie Totterstrom <sophie.totterstrom@gmail.com>
2025-08-15 15:03:59 +0300

    add own helper bash script for setup in csc supercomputers

---
+++ env_setup_csc/env_avsegformer_post_install.sh
@@ -0,0 +1,11 @@
+pip install torch==1.10.0+cu111 torchvision==0.11.0+cu111 torchaudio==0.10.0 -f https://download.pytorch.org/whl/torch_stable.html
+pip install mmcv-full -f https://download.openmmlab.com/mmcv/dist/cu111/torch1.10.0/index.html
+pip install pandas
+pip install timm
+pip install resampy
+pip install soundfile
+#pip install -U setuptools
+#pip install ninja
+
+cd /scratch/project_2005102/sophie/repos/AVSegFormer/ops
+sh make.sh
