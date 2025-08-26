b4cb814b348b9987686d02e97e1d5f4ce8addea5
Sophie Totterstrom <sophie.totterstrom@gmail.com>
2025-08-26 09:28:34 +0300

    add separate train script for sav dataset

--- train.sh
+++ train.sh
@@ -1,5 +1,8 @@
-SESSION=$1
-CONFIG=$2
+SESSION="ms3" #$1
+CONFIG="config/sav/AVSegFormer_pvt2_ms3_sav.py" #$2
+#TRAIN_FILE="train.py"
+TRAIN_FILE="sav_train.py"
+
 
 PYTHONPATH="$(dirname $0)/..":$PYTHONPATH \
-python scripts/$SESSION/train.py $CONFIG
+python scripts/$SESSION/$TRAIN_FILE $CONFIG
