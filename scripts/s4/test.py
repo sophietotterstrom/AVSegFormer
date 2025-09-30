65402133c40bf5681a6d6cb2ff330fdcbe4aa3e1
Sophie Tötterström <sophie.totterstrom@gmail.com>
2025-09-30 15:01:51 +0300

    save eval info for further analysis

--- scripts/s4/test.py
+++ scripts/s4/test.py
@@ -8,7 +8,7 @@ from utility import mask_iou, Eval_Fmeasure, save_mask
 from utils.logger import getLogger
 from model import build_model
 from dataloader import build_dataset
-
+import csv
 
 def main():
     # logger
@@ -37,41 +37,53 @@ def main():
     avg_meter_miou = pyutils.AverageMeter('miou')
     avg_meter_F = pyutils.AverageMeter('F_score')
 
-    # Test
-    with torch.no_grad():
-        for n_iter, batch_data in enumerate(test_dataloader):
-            # [bs, 5, 3, 224, 224], [bs, 5, 1, 96, 64], [bs, 1, 1, 224, 224]
-            imgs, audio, mask, category_list, video_name_list = batch_data
 
-            imgs = imgs.cuda()
-            audio = audio.cuda()
-            mask = mask.cuda()
-            B, frame, C, H, W = imgs.shape
-            imgs = imgs.view(B * frame, C, H, W)
-            mask = mask.view(B * frame, H, W)
-            audio = audio.view(-1, audio.shape[2],
-                               audio.shape[3], audio.shape[4])
 
-            output, _ = model(audio, imgs)
-            if args.save_pred_mask:
-                mask_save_path = os.path.join(
-                    args.save_dir, dir_name, 'pred_masks')
-                save_mask(output.squeeze(1), mask_save_path,
-                          category_list, video_name_list)
-                logger.info(f"INSIDE IF, save to {mask_save_path}, {video_name_list}")
+    csv_path = os.path.join(args.save_dir, dir_name, 'pred_masks')
+    write_file = os.path.join(csv_path, "output.csv")
+    with open(write_file, mode="w", newline="") as f:
+        writer = csv.writer(f)
+        writer.writerow(["iter", "video_name", "category", "miou", "F_score"])  # header
+    
 
-            miou = mask_iou(output.squeeze(1), mask)
-            avg_meter_miou.add({'miou': miou})
-            F_score = Eval_Fmeasure(output.squeeze(1), mask)
-            avg_meter_F.add({'F_score': F_score})
-            logger.info('n_iter: {}, iou: {}, F_score: {}'.format(
-                n_iter, miou, F_score))
+        # Test
+        with torch.no_grad():
+            for n_iter, batch_data in enumerate(test_dataloader):
+                # [bs, 5, 3, 224, 224], [bs, 5, 1, 96, 64], [bs, 1, 1, 224, 224]
+                imgs, audio, mask, category_list, video_name_list = batch_data
 
-        miou = (avg_meter_miou.pop('miou'))
-        F_score = (avg_meter_F.pop('F_score'))
-        logger.info(f'test miou: {miou.item}')
-        logger.info(f'test F_score: {F_score}')
-        logger.info('test miou: {}, F_score: {}'.format(miou.item(), F_score))
+                imgs = imgs.cuda()
+                audio = audio.cuda()
+                mask = mask.cuda()
+                B, frame, C, H, W = imgs.shape
+                imgs = imgs.view(B * frame, C, H, W)
+                mask = mask.view(B * frame, H, W)
+                audio = audio.view(-1, audio.shape[2],
+                                audio.shape[3], audio.shape[4])
+
+                output, _ = model(audio, imgs)
+                if args.save_pred_mask:
+                    mask_save_path = os.path.join(
+                        args.save_dir, dir_name, 'pred_masks')
+                    save_mask(output.squeeze(1), mask_save_path,
+                            category_list, video_name_list)
+                    logger.info(f"INSIDE IF, save to {mask_save_path}, {video_name_list}")
+
+                miou = mask_iou(output.squeeze(1), mask)
+                avg_meter_miou.add({'miou': miou})
+                F_score = Eval_Fmeasure(output.squeeze(1), mask)
+                avg_meter_F.add({'F_score': F_score})
+                logger.info(f'n_iter: {n_iter}, iou: {miou}, F_score: {F_score}')
+
+                writer.writerow([n_iter, mask_save_path, video_name_list, category_list, miou.item, F_score])
+
+                
+
+            miou = (avg_meter_miou.pop('miou'))
+            F_score = (avg_meter_F.pop('F_score'))
+            logger.info(f'test miou: {miou.item}')
+            logger.info(f'test F_score: {F_score}')
+            logger.info('test miou: {}, F_score: {}'.format(miou.item(), F_score))
 
 
 if __name__ == '__main__':
