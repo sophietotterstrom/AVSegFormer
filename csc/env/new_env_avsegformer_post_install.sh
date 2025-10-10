a80e521082921ae562e165d4748de2bd806c3386
Sophie Totterstrom <sophie.totterstrom@gmail.com>
2025-10-10 08:41:01 +0300

    env post install script changes (add version for torch 1.11)

--- csc/env/new_env_avsegformer_post_install.sh
+++ csc/env/new_env_avsegformer_post_install.sh
@@ -1,7 +1,8 @@
 # 1. module load tykky
 # 2. conda-containerize update /scratch/project_2005102/sophie/segformer_conda --post-install /scratch/project_2005102/sophie/repos/AVSegFormer/csc/env/env_avsegformer_post_install.sh
 
-pip install torch==1.10.0+cu111 torchvision==0.11.0+cu111 torchaudio==0.10.0 -f https://download.pytorch.org/whl/torch_stable.html
+pip install torch==1.11.0+cu113 torchvision==0.12.0+cu113 torchaudio==0.11.0 --extra-index-url https://download.pytorch.org/whl/cu113
+
 pip install mmcv-full -f https://download.openmmlab.com/mmcv/dist/cu111/torch1.10.0/index.html
 pip install pandas
 pip install timm
@@ -14,8 +15,10 @@ pip install pycocotools
 
 pip install "opencv-python>=4.10,<4.11"
 
-pip install mlflow
+# tensorboard
+pip install tensorboard
 
-#  comment these out after first time building the env (also when running on non-GPU node)
+
+# for first time running this post-install script, comment these out
 cd /scratch/project_2005102/sophie/repos/AVSegFormer/ops
 sh make.sh
