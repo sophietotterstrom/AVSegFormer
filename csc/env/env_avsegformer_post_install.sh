0610b370946eb9f245aa1f17f74e4e9c1e911373
Sophie Tötterström <sophie.totterstrom@gmail.com>
2025-10-10 08:26:38 +0300

    add mlflow requirement since support is now in some DPP training scripts

--- csc/env/env_avsegformer_post_install.sh
+++ csc/env/env_avsegformer_post_install.sh
@@ -11,6 +11,8 @@ pip install pycocotools
 
 pip install "opencv-python>=4.10,<4.11"
 
+pip install mlflow
+
 # for first time running this post-install script, comment these out
 #cd /scratch/project_2005102/sophie/repos/AVSegFormer/ops
 #sh make.sh
